0c8c9a95df31d2efcef7ca2741f604d455447f36
TAUFER Kevin <kevin.taufer@hexagon.com>
2022-04-20 22:12:32 +0200

    removed cors

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,9 @@ const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
-const cors = require("cors");
+
 const { errorHandler } = require("./middleware/errorMiddleware");
 const cron = require("node-cron");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
 const connectDB = require("./config/db");
 const { sendEmail } = require("./email");
@@ -14,17 +12,13 @@ const { getReminders } = require("./controllers/appointmentController");
 
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
 
 // Connect to database
 connectDB();
 
 const app = express();
 
-app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
